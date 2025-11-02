a7aa26a530e8296a107ffdb4257c52a0bff700e7
raghu762 <errabelliraghu99634@gmail.com>
2025-11-02 20:25:42 +0530

    Build and Deploy react app in EKS

---
+++ my-react-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My First React App 🚀</h1>
+        <p>This is running locally on my system!</p>
+        <a
+          className="App-link"
+          href="https://github.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit My GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
